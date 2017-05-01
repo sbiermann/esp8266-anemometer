cad56d0d1e6bfe8108d00a7621cff569f5ed5f24
sbn <sb@ems-solutions.com>
2017-05-01 22:17:45 +0200

    sending only 10s intervall is wind is not changing more than 1 km/h

--- esp8266-anemometer.ino
+++ esp8266-anemometer.ino
@@ -8,6 +8,8 @@
 unsigned long  next_timestamp = 0;
 volatile unsigned long i = 0;
 float wind = 0;
+float last_wind = 0;
+int count = 0;
 volatile unsigned long last_micros;
 long debouncing_time = 5; //in millis
 int input_pin = 13;
@@ -60,22 +62,27 @@ void loop()
 {
   if (millis() > next_timestamp )    
   { 
-    detachInterrupt(input_pin); 
+    detachInterrupt(input_pin);
+    count++; 
     float rps = i/number_reed; //computing rounds per second 
     if(i == 0)
       wind = 0.0;
     else
       wind = 1.761 / (1 + rps) + 3.013 * rps;// found here: https://www.amazon.de/gp/customer-reviews/R3C68WVOLJ7ZTO/ref=cm_cr_getr_d_rvw_ttl?ie=UTF8&ASIN=B0018LBFG8 (in German)
-    if(debugOutput){
-      Serial.print("Wind: ");
-      Serial.print(wind);
-      Serial.println(" km/h");
+    if(last_wind - wind > 1 || wind - last_wind < -1 || count >= 10){
+      if(debugOutput){
+        Serial.print("Wind: ");
+        Serial.print(wind);
+        Serial.println(" km/h");
+      }
+      String strBuffer;
+      strBuffer =  String(wind);
+      strBuffer.toCharArray(charBuffer,10);
+      client.publish(mqtt_topic_prefix, charBuffer, false);
+      count = 0;
     }
-    String strBuffer;
-    strBuffer =  String(wind);
-    strBuffer.toCharArray(charBuffer,10);
-    client.publish(mqtt_topic_prefix, charBuffer, false);
     i = 0;
+    last_wind = wind;
     next_timestamp  = millis()+1000; //intervall is 1s
     attachInterrupt(input_pin,Interrupt,RISING);
   }
